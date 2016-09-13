aea43177939c13b9e21a43057821d7f99df2ecbf
BRHGreen <benedictgreen@gmail.com>
2016-09-13 10:48:14 +0100

    Map renderd. Basic gulp ok. Login frontend started

--- restaurants-main/src/js/app.js
+++ restaurants-main/src/js/app.js
@@ -1,13 +1,47 @@
+
 const App = App || {};
 
-App.init = function (){
-  $("#register").on("click", this.resgister.bind(this));
-  $("#login").on("click", this.login.bind(this));
+App.init = function() {
+  $('#register').on('click', a);
+  $('#login').on('click', this.login.bind(this));
 
-  this.apiUrl = 'http://localost:3000/api';
+  this.apiUrl = 'http://localhost:3000/api';
   this.$main = $('main');
 
   this.$main.on('submit', 'form', this.handleForm);
 };
 
-console.log(App);
+// App.register = function() {
+//   event.preventDefault();
+//   console.log('anything?');
+// };
+
+function a() {
+  console.log('???');
+}
+
+
+
+
+
+
+
+const googleMap = googleMap || {};
+
+googleMap.mapSetup = function() {
+  let canvas = document.getElementById('map-canvas');
+  let mapOptions = {
+    zoom: 12,
+    center: new google.maps.LatLng(51.506178,-0.088369),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  this.map = new google.maps.Map(canvas, mapOptions);
+};
+
+$(googleMap.mapSetup.bind(googleMap));
+
+$('#login').on('click', function() {
+  console.log('yo');
+});
+
+$(App.init.bind(App));
