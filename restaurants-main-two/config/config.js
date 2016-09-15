e722705a3305a3f89c64bb9e51c2105f3e67c92a
BRHGreen <benedictgreen@gmail.com>
2016-09-15 21:40:07 +0100

    this one works. DON'T CHANGE A THING

--- restaurants-main-two/config/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  port: process.env.PORT || 3000,
-  db: 'mongodb://localhost/vegan-london',
-  secret: process.env.SECRET || "this is secret. You have no idea what its purpose is"
-};
