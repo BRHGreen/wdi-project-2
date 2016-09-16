6e4f6200b0a5b3539e91119f584af9d155065301
BRHGreen <benedictgreen@gmail.com>
2016-09-16 11:23:19 +0100

    mongodb_uri added to config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGOLAB_URI || 'mongodb://localhost/vegan-london',
+  db: process.env.MONGODB_URI || 'mongodb://localhost/vegan-london',
   secret: process.env.SECRET || "this is secret. You have no idea what its purpose is"
 };
