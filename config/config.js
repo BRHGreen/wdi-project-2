4e3afa0324bc65a5a33230b21ee764c9a12ee130
BRHGreen <benedictgreen@gmail.com>
2016-11-17 15:32:21 +0000

    seeded heroku

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGODB_URI || 'mongodb://localhost/vegan-london',
+  // db: process.env.MONGODB_URI || 'mongodb://localhost/vegan-london',
+  db: 'mongodb://benedictgreen:veganlondon@ds157247.mlab.com:57247/heroku_h1cc80mf',
   secret: process.env.SECRET || "this is secret. You have no idea what its purpose is"
 };
